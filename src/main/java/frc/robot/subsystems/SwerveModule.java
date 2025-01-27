00bc3f4d496fccedf5f88b06e92d329e882fdcb5
Aras ARKAN <arasarkan9@gmail.com>
2025-01-28 02:17:50 +0300

    pid values test

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -2,15 +2,8 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
-
-import java.io.ObjectInputFilter.Config;
-
-import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.configs.TalonFXConfigurator;
 import com.ctre.phoenix6.hardware.CANcoder;
-import com.ctre.phoenix6.hardware.DeviceIdentifier;
-
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -27,13 +20,12 @@ public class SwerveModule extends SubsystemBase {
     private PIDController steerPID; 
     private PIDController drivePID; 
 
-    private final double wheelDiameter; 
 
     public SwerveModule(int driveMotorPort, int steerMotorPort, int steerEncoderPort, boolean isDriveMotorInverted, boolean isSteerMotorInverted) {
         driveMotor = new TalonFX(driveMotorPort);
         driveMotorConfiguration = new TalonFXConfiguration();
         driveMotorConfiguration.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
-        driveMotor.getConfigurator().apply(driveMotorConfiguration);
+        driveMotor.getConfigurator().apply(driveMotorConfiguration.Slot0);
         
         this.steerMotor = new TalonFX(steerMotorPort);
         steerMotorConfiguration = new TalonFXConfiguration();
@@ -42,9 +34,33 @@ public class SwerveModule extends SubsystemBase {
         
         this.steerEncoder = new CANcoder(steerEncoderPort);
 
-        this.wheelDiameter = Constants.SwerveDrive.WHEEL_DIAMETER;
         this.drivePID = new PIDController(Constants.SwerveDrive.DriveMotorPID.P, Constants.SwerveDrive.DriveMotorPID.I, Constants.SwerveDrive.DriveMotorPID.D);
         this.steerPID = new PIDController(Constants.SwerveDrive.SteerMotorPID.P, Constants.SwerveDrive.SteerMotorPID.I, Constants.SwerveDrive.SteerMotorPID.D);
     }
 
+    public void configurePIDForAutonomous() {
+        driveMotorConfiguration.Slot0.kP = Constants.SwerveDrive.DriveMotorPID.P;
+        driveMotorConfiguration.Slot0.kI = Constants.SwerveDrive.DriveMotorPID.I;
+        driveMotorConfiguration.Slot0.kD = Constants.SwerveDrive.DriveMotorPID.D;
+
+        steerMotorConfiguration.Slot0.kP = Constants.SwerveDrive.SteerMotorPID.P;
+        steerMotorConfiguration.Slot0.kI = Constants.SwerveDrive.SteerMotorPID.I;
+        steerMotorConfiguration.Slot0.kD = Constants.SwerveDrive.SteerMotorPID.D;
+
+        driveMotor.getConfigurator().apply(driveMotorConfiguration.Slot0);
+        steerMotor.getConfigurator().apply(steerMotorConfiguration.Slot0);
+    }
+
+    public void resetPIDToDefault() {
+        driveMotorConfiguration.Slot0.kP = Constants.SwerveDrive.DriveMotorPID.P;
+        driveMotorConfiguration.Slot0.kI = Constants.SwerveDrive.DriveMotorPID.I;
+        driveMotorConfiguration.Slot0.kD = Constants.SwerveDrive.DriveMotorPID.D;
+
+        steerMotorConfiguration.Slot0.kP = Constants.SwerveDrive.SteerMotorPID.P;
+        steerMotorConfiguration.Slot0.kI = Constants.SwerveDrive.SteerMotorPID.I;
+        steerMotorConfiguration.Slot0.kD = Constants.SwerveDrive.SteerMotorPID.D;
+
+        driveMotor.getConfigurator().apply(driveMotorConfiguration.Slot0);
+        steerMotor.getConfigurator().apply(steerMotorConfiguration.Slot0);
+    }
 }
