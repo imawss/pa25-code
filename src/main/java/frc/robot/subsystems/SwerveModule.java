91bb85a18a1f00a71df5c721e7f007bb530a4c82
Aras ARKAN <arasarkan9@gmail.com>
2025-02-05 22:54:43 +0300

    Swerve Module COMPLETED!

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -10,61 +10,90 @@ import static edu.wpi.first.units.Units.RotationsPerSecond;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.CANcoder;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.math.Conversions;
 import frc.robot.Constants;
 
 public class SwerveModule extends SubsystemBase {
-    private TalonFX driveMotor;  
+    private TalonFX driveMotor;
     private TalonFXConfiguration driveMotorConfiguration;
 
-    private TalonFX steerMotor;  
+    private TalonFX steerMotor;
     private TalonFXConfiguration steerMotorConfiguration;
 
-    private RelativeEncoder driveEncoder;
-    private CANcoder steerEncoder; 
+    private CANcoder steerEncoder;
 
-    private PIDController steerPID; 
-    private PIDController drivePID; 
+    private PIDController steerPID;
+    private PIDController drivePID;
 
-
-    public SwerveModule(int driveMotorPort, int steerMotorPort, int steerEncoderPort, boolean isDriveMotorInverted, boolean isSteerMotorInverted) {
+    public SwerveModule(int driveMotorPort, int steerMotorPort, int steerEncoderPort, boolean isDriveMotorInverted,
+            boolean isSteerMotorInverted) {
         driveMotor = new TalonFX(driveMotorPort);
         driveMotorConfiguration = new TalonFXConfiguration();
-        driveMotorConfiguration.MotorOutput.Inverted = isDriveMotorInverted ? InvertedValue.CounterClockwise_Positive : InvertedValue.Clockwise_Positive;
+        driveMotorConfiguration.MotorOutput.Inverted = isDriveMotorInverted ? InvertedValue.CounterClockwise_Positive
+                : InvertedValue.Clockwise_Positive;
         driveMotorConfiguration.Feedback.SensorToMechanismRatio = Constants.SwerveDrive.kGearRatio;
         driveMotor.getConfigurator().apply(driveMotorConfiguration);
-        
+
         this.steerMotor = new TalonFX(steerMotorPort);
         steerMotorConfiguration = new TalonFXConfiguration();
-        steerMotorConfiguration.MotorOutput.Inverted = isSteerMotorInverted ? InvertedValue.CounterClockwise_Positive : InvertedValue.Clockwise_Positive;
+        steerMotorConfiguration.MotorOutput.Inverted = isSteerMotorInverted ? InvertedValue.CounterClockwise_Positive
+                : InvertedValue.Clockwise_Positive;
         steerMotor.getConfigurator().apply(steerMotorConfiguration);
 
         this.steerEncoder = new CANcoder(steerEncoderPort);
-  
-        this.drivePID = new PIDController(Constants.SwerveDrive.DriveMotorPID.kP, Constants.SwerveDrive.DriveMotorPID.kD, Constants.SwerveDrive.DriveMotorPID.kD);
-        this.steerPID = new PIDController(Constants.SwerveDrive.SteerMotorPID.kP, Constants.SwerveDrive.SteerMotorPID.kI, Constants.SwerveDrive.SteerMotorPID.kD);
+
+        this.drivePID = new PIDController(Constants.SwerveDrive.DriveMotorPID.kP,
+                Constants.SwerveDrive.DriveMotorPID.kI, Constants.SwerveDrive.DriveMotorPID.kD);
+        this.steerPID = new PIDController(Constants.SwerveDrive.SteerMotorPID.kP,
+                Constants.SwerveDrive.SteerMotorPID.kI, Constants.SwerveDrive.SteerMotorPID.kD);
+        steerPID.enableContinuousInput(-Math.PI, Math.PI);
     }
 
     public double getDrivePosition() {
-        return Conversions.rotationsToMeters(driveMotor.getPosition().getValue().in(Rotations), Constants.SwerveDrive.kWheelCircumference);
+        return Conversions.rotationsToMeters(driveMotor.getPosition().getValue().in(Rotations),
+                Constants.SwerveDrive.kWheelCircumference);
     }
 
     public double getDriveVelocity() {
-        return Conversions.RPSToMPS(driveMotor.getVelocity().getValue().in(RotationsPerSecond), Constants.SwerveDrive.kWheelCircumference);
+        return Conversions.RPSToMPS(driveMotor.getVelocity().getValue().in(RotationsPerSecond),
+                Constants.SwerveDrive.kWheelCircumference);
     }
 
     public double getSteerAngle() {
-        return steerEncoder.getAbsolutePosition().getValue().in(Rotations) * Constants.SwerveDrive.kTurningEncoderRot2Rad;
+        return steerEncoder.getAbsolutePosition().getValue().in(Rotations)
+                * Constants.SwerveDrive.kTurningEncoderRot2Rad;
     }
 
     public double getSteerVelocity() {
-        return Conversions.RPSToMPS(steerEncoder.getVelocity().getValue().in(RotationsPerSecond), Constants.SwerveDrive.kWheelCircumference);
+        return Conversions.RPSToMPS(steerEncoder.getVelocity().getValue().in(RotationsPerSecond),
+                Constants.SwerveDrive.kWheelCircumference);
     }
 
-    public void stopModule(){
+    public void stopModule() {
         driveMotor.set(0);
         steerMotor.set(0);
+    }
+
+    public void resetEncoders() {
+        driveMotor.setPosition(0);
+        steerEncoder.setPosition(steerEncoder.getAbsolutePosition().getValue());
+    }
+
+    public void setDesiredState(SwerveModuleState state) {
+        if (Math.abs(state.speedMetersPerSecond) < 0.001) {
+            stopModule();
+            return;
+        }
+        state.optimize(new Rotation2d(getSteerAngle()));
+        double driveOutput = drivePID.calculate(getDriveVelocity(), state.speedMetersPerSecond);
+        driveMotor.set(driveOutput);
+        double steerOutput = steerPID.calculate(getSteerAngle(), state.angle.getRadians());
+        steerMotor.set(steerOutput);
+        SmartDashboard.putString("Swerve[" + steerEncoder.getDeviceID() + "] state", state.toString());
     }
 
     public void configurePIDForAutonomous(double kPD, double kID, double kDD, double kPS, double kIS, double kDS) {
